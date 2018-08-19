a3d224f484277cdd1a18bd6f1dd6e3b032f30a22
JamesACM <jaimecanalesmarin@gmail.com>
2018-08-19 05:57:42 -0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "admin",password: "admin123")
+Product.create(title: "Metal Gear Solid", description:"metal gear...", image_url: "mgs.jpg", price: 20)
+Product.create(title: "Street Fighter V", description:"juego de luchas capcom...", image_url: "sfv.jpg", price: 10)
+Product.create(title: "The king of Fighters XIII", description:"juego de luchas snk...", image_url: "kof_xiii.jpg", price: 20)
+
